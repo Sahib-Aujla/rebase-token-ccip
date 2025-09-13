cef1716b17c7cb487fb14581a5cd6c4005770f91
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-13 13:30:23 +0530

    added vault contract

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,6 +3,34 @@ pragma solidity ^0.8.24;
 
 import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
-contract Vault{
+contract Vault {
+    error Vault__RedeemTransferFailed();
 
+    event Deposit(address to, uint256 amount);
+    event Redeem(address from, uint256 amount);
+
+    IRebaseToken private immutable i_rebaseToken;
+
+    constructor(address rebaseTokenAddress) {
+        i_rebaseToken = IRebaseToken(rebaseTokenAddress);
+    }
+
+    function deposit(uint256 amount) external payable {
+        i_rebaseToken.mint(msg.sender, amount);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    receive() external payable {}
+
+    function redeem(uint256 amount) external {
+        if (amount == type(uint256).max) {
+            amount = i_rebaseToken.balanceOf(msg.sender);
+        }
+        i_rebaseToken.burn(msg.sender, amount);
+        emit Redeem(msg.sender, amount);
+        (bool success,) = payable(msg.sender).call{value: amount}("");
+        if (!success) {
+            revert Vault__RedeemTransferFailed();
+        }
+    }
 }
