524112b7aa461d359ba9ccbdc32ff1ad69fa11ec
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-14 10:22:58 +0530

    added chainlink-local

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -1,7 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {Test} from "forge-std/Test.sol";
 
-contract CrossChain {
-   
+contract CrossChain is Test {
+    uint256 sepoliaFork;
+    uint256 arbSepoliaFork;
+
+    function setup() external {
+        sepoliaFork = vm.createSelectFork("eth");
+        arbSepoliaFork = vm.createFork("arb");
+    }
 }
