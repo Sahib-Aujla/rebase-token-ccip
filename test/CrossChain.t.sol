3d7bbea57de722d1a436f5724b44e93311e03211
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-14 10:38:42 +0530

    added pool contracts

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -8,6 +8,8 @@ import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {Vault} from "../src/Vault.sol";
 
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
 contract CrossChain is Test {
     address owner = makeAddr("owner");
     uint256 sepoliaFork;
@@ -19,6 +21,12 @@ contract CrossChain is Test {
     RebaseToken arbSepoliaRebaseToken;
     Vault vault;
 
+    Register.NetworkDetails sepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
+
+    RebaseTokenPool sepoliaPool;
+    RebaseTokenPool arbSepoliaPool;
+
     function setup() external {
         sepoliaFork = vm.createSelectFork("eth");
         arbSepoliaFork = vm.createFork("arb");
@@ -26,14 +34,28 @@ contract CrossChain is Test {
         ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
         vm.makePersistent(address(ccipLocalSimulatorFork));
 
+        sepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
         vm.startPrank(owner);
         sepoliaRebaseToken = new RebaseToken();
         vault = new Vault(address(sepoliaRebaseToken));
+        sepoliaPool = new RebaseTokenPool(
+            IERC20(address(sepoliaRebaseToken)),
+            new address[](0),
+            sepoliaNetworkDetails.rmnProxyAddress,
+            sepoliaNetworkDetails.routerAddress
+        );
         vm.stopPrank();
 
         vm.selectFork(arbSepoliaFork);
+        arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
         vm.startPrank(owner);
         arbSepoliaRebaseToken = new RebaseToken();
+        arbSepoliaPool = new RebaseTokenPool(
+            IERC20(address(arbSepoliaRebaseToken)),
+            new address[](0),
+            arbSepoliaNetworkDetails.rmnProxyAddress,
+            arbSepoliaNetworkDetails.routerAddress
+        );
         vm.stopPrank();
     }
 }
